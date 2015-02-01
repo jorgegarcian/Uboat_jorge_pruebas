a0e956a5a0bff22a9a1b8148f847524cb1d67812
Jorge Garcia <jorge@Mac-Pro-de-jorge.local>
2015-02-01 03:38:38 +0100

    espaCIO

--- UBoat/Menu.swift
+++ UBoat/Menu.swift
@@ -24,6 +24,7 @@ import SpriteKit
        
         
     }
+        
     
     
     func imagenPeriscopio() {
@@ -34,6 +35,7 @@ import SpriteKit
         addChild(fondo)
         
     }
+        
     
     
     
